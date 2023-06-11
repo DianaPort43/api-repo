682639c33cb2d19371251864814d585b08003212
DianaPort43 <129172905+DianaPort43@users.noreply.github.com>
2023-06-11 14:05:30 +0600

    finish js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let gridContainer = document.querySelector('.grid-container');
-let searchBtn = document.querySelector(".searchBtn");
-
 async function searchCountries() {
   var searchKeyword = document.querySelector('#searchInput').value;
   var url = 'https://restcountries.com/v3.1/name/' + searchKeyword;
@@ -11,6 +8,38 @@ async function searchCountries() {
     } else {
       gridContainer.innerHTML = '';
 
+      data.forEach(function(country) {
+        var countryName = country.name.common;
+        var flagUrl = country.flags.svg;
+        var language = Object.values(country.languages)[0];
+        var startOfWeek = country.startOfWeek;
+        var capital = country.capital;
+
+        var countryDiv = document.createElement('div');
+        countryDiv.classList.add('country-card');
+
+        var flagImg = document.createElement('img');
+        flagImg.src = flagUrl;
+        flagImg.alt = countryName + ' flag';
+        countryDiv.appendChild(flagImg);
+
+        var nameElement = document.createElement('h3');
+        nameElement.textContent = 'Name Of Country: ' + countryName;
+        countryDiv.appendChild(nameElement);
+
+        var nameElement = document.createElement('h4');
+        nameElement.textContent = 'Capital: ' + capital;
+        countryDiv.appendChild(nameElement);
+
+        var languageElement = document.createElement('h4');
+        languageElement.textContent = 'Official language: ' + language;
+        countryDiv.appendChild(languageElement);
+
+        var startOfWeekElement = document.createElement('h4');
+        startOfWeekElement.textContent = 'Start Of Week: ' + startOfWeek;
+        countryDiv.appendChild(startOfWeekElement);
+
+        gridContainer.appendChild(countryDiv);
       });
     }
 }
