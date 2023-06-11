ae9798748d53ec76ea77e1fe4cf842f6a6116e9c
DianaPort43 <129172905+DianaPort43@users.noreply.github.com>
2023-06-11 14:02:29 +0600

    add async func

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 let gridContainer = document.querySelector('.grid-container');
 let searchBtn = document.querySelector(".searchBtn");
 
+async function searchCountries() {
+  var searchKeyword = document.querySelector('#searchInput').value;
+  var url = 'https://restcountries.com/v3.1/name/' + searchKeyword;
+  const response = await fetch(url);
+  var data = await response.json();
+  if (data === null || data.length === 0) {
+      gridContainer.innerHTML = "Нет результатов поиска. Попробуйте еще раз!";
+    } else {
+      gridContainer.innerHTML = '';
+
+      });
+    }
+}
+
+searchBtn.addEventListener('click', searchCountries);
