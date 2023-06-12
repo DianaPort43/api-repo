ab5197403836e12c3732e7ef8430be4893486a57
DianaPort43 <129172905+DianaPort43@users.noreply.github.com>
2023-06-12 17:42:17 +0600

    add try and catch

--- script.js
+++ script.js
@@ -1,47 +1,62 @@
+let searchBtn = document.querySelector('.search-btn');
+let countryInp = document.querySelector('#searchInput');
+let result = document.querySelector('#result');
+
 async function searchCountries() {
-  var searchKeyword = document.querySelector('#searchInput').value;
-  var url = 'https://restcountries.com/v3.1/name/' + searchKeyword;
-  const response = await fetch(url);
-  var data = await response.json();
-  if (data === null || data.length === 0) {
-      gridContainer.innerHTML = "Нет результатов поиска. Попробуйте еще раз!";
-    } else {
-      gridContainer.innerHTML = '';
+  let countryName = countryInp.value;
 
-      data.forEach(function(country) {
-        var countryName = country.name.common;
-        var flagUrl = country.flags.svg;
-        var language = Object.values(country.languages)[0];
-        var startOfWeek = country.startOfWeek;
-        var capital = country.capital;
+  try {
+    var url = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+    const response = await fetch(url);
 
-        var countryDiv = document.createElement('div');
-        countryDiv.classList.add('country-card');
-
-        var flagImg = document.createElement('img');
-        flagImg.src = flagUrl;
-        flagImg.alt = countryName + ' flag';
-        countryDiv.appendChild(flagImg);
-
-        var nameElement = document.createElement('h3');
-        nameElement.textContent = 'Name Of Country: ' + countryName;
-        countryDiv.appendChild(nameElement);
-
-        var nameElement = document.createElement('h4');
-        nameElement.textContent = 'Capital: ' + capital;
-        countryDiv.appendChild(nameElement);
-
-        var languageElement = document.createElement('h4');
-        languageElement.textContent = 'Official language: ' + language;
-        countryDiv.appendChild(languageElement);
-
-        var startOfWeekElement = document.createElement('h4');
-        startOfWeekElement.textContent = 'Start Of Week: ' + startOfWeek;
-        countryDiv.appendChild(startOfWeekElement);
-
-        gridContainer.appendChild(countryDiv);
-      });
+    if (!response.ok) {
+      throw new Error('Ошибка запроса');
     }
+
+    var data = await response.json();
+
+    if (data === null || data.length === 0) {
+      result.innerHTML = "No search results. Please try again!";
+    } else {
+      result.innerHTML = `
+            <img src="${data[0].flags.png}" class="flag-img">
+            <h2>${data[0].name.common}</h2>
+            <div class="wrapper">
+                <div class="data-wrapper">
+                    <h4>Capital:</h4>
+                    <span>${data[0].capital[0]}</span>
+                </div>
+            </div>
+            <div class="wrapper">
+                <div class="data-wrapper">
+                    <h4>Continent:</h4>
+                    <span>${data[0].continent}</span>
+                </div>
+            </div>
+            <div class="wrapper">
+                <div class="data-wrapper">
+                    <h4>Population:</h4>
+                    <span>${data[0].population}</span>
+                </div>
+            </div>
+            <div class="wrapper">
+                <div class="data-wrapper">
+                    <h4>Currency:</h4>
+                    <span>${data[0].currencies[Object.keys(data[0].currencies)[0]].name} - ${Object.keys(data[0].currencies)[0]}</span>
+                </div>
+            </div> 
+            <div class="wrapper">
+                <div class="data-wrapper">
+                    <h4>Common Languages:</h4>
+                    <span>${Object.values(data[0].languages).join(', ')}</span>
+                </div>
+            </div>    
+      `;
+    }
+  } catch (error) {
+    console.error(error);
+    result.innerHTML = "Enter full country name";
+  }
 }
 
 searchBtn.addEventListener('click', searchCountries);
